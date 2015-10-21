57af4c258f35a70b7064985d6aa352a89dbee8cf
Julian R. Moore <jreedmoore@gmail.com>
2015-10-21 00:00:18 +0000

    Add unformatted event creation form

--- src/cljs/event_planner/core.cljs
+++ src/cljs/event_planner/core.cljs
@@ -24,9 +24,10 @@
           :error-handler #(dispatch [:event-response %1])
           :format :json
           :params {:recurring true
-                 :days-of-week ["Monday"]
-                 :start-time 9
-                 :end-time 17}})
+                   :title "Some Time in the Future"
+                   :days-of-week ["Monday"]
+                   :start-time 9
+                   :end-time 17}})
    (update-in db [:clicks] inc)))
 
 (register-handler
@@ -49,6 +50,46 @@
    [db _]
    (reaction (:data @db))))
 
+(def days ["Sunday"
+           "Monday"
+           "Tuesday"
+           "Wednesday"
+           "Thursday"
+           "Friday"
+           "Saturday"])
+
+(defn checkbox
+  [val name]
+  [:input {:type "checkbox" :value val} name])
+
+(defn drop-down
+  [vals]
+  [:select
+   (for [[key name] vals]
+     [:option {:value key} name])])
+
+(defn military->12h [hour]
+  (if (> hour 12)
+    (str (- hour 12) " pm")
+    (str hour " am")))
+
+;; FIXME time is hard
+(defn time-drop-down
+  []
+  (let [hours (map inc (range 24))]
+    [drop-down (map vector
+                    hours
+                    (map military->12h hours))]))
+
+(defn recurring-event-form
+  []
+  [:form
+   "Title:" [:input {:type "text"}]
+   "Days of Week:" (for [day days] [checkbox "help" day])
+   "Start Time:" [time-drop-down]
+   "End Time:" [time-drop-down]])
+
+
 (defn simple-component []
   (let [clicks (subscribe [:clicks])
         data (subscribe [:data])]
@@ -60,7 +101,10 @@
       "I have " [:em "bold"]
       [:span {:style {:color "red"}} " and red "] "text."]
      [:button {:type "button"
-               :on-click #(dispatch [:clicked])} "Click Me!"]]))
+               :on-click #(dispatch [:clicked])} "Click Me!"]
+     [recurring-event-form]]))
+
+
 
 (defn render-simple []
   (r/render-component [simple-component]
