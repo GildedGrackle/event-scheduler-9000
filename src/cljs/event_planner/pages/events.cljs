9fe0b25392080efaab80c15be0e4d337a412fcdc
Morgan Hopeman <mwhopeman@wpi.edu>
2015-10-23 17:18:10 -0400

    Edit event page

--- src/cljs/event_planner/pages/events.cljs
+++ src/cljs/event_planner/pages/events.cljs
@@ -1,9 +1,8 @@
 (ns event-planner.pages.events)
 
 (defn events-page [id]
-  [:div "Wow my id is: " id]
   [:div.container
-    [:div.row.col-md-4
+    [:div.row.col-xs-4
       [:form.form-signin
         [:h2.form-signin-heading "Sign In"]
         [:label {:for "inputusername"
@@ -19,8 +18,9 @@
                  :id "inputpassword"
                  :placeholder "Password"}]
         [:button.btn.btn-sm.btn-primary {:type "Submit"} "Sign In"]]
-    [:p ]
-    [:p "Please note that all sign in usernames and passwords are temporary."]
-    [:p "To sign in using your account please go to the "
-        [:a {:href "#"} "Login"] " page."]
-   ]])
+      [:p ]
+      [:p "Please note that all sign in usernames and passwords are temporary."]
+      [:p "To sign in using your account please go to the "
+        [:a {:href "#"} "Login"] " page."]]
+    [:div.row.col-xs-8
+     [:h1 "HELL YEAH"]]])
