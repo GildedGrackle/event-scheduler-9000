3729392bd2fff3f479b6a5bcaea7c562d814bbbb
Morgan Hopeman <mwhopeman@wpi.edu>
2015-10-23 14:37:48 -0400

    Create Temporary Log-in for event page

--- src/cljs/event_planner/pages/events.cljs
+++ src/cljs/event_planner/pages/events.cljs
@@ -1,4 +1,26 @@
 (ns event-planner.pages.events)
 
 (defn events-page [id]
-  [:div "Wow my id is: " id])
+  [:div "Wow my id is: " id]
+  [:div.container
+    [:div.row.col-md-4
+      [:form.form-signin
+        [:h2.form-signin-heading "Sign In"]
+        [:label {:for "inputusername"
+                 :class "sr-only"} "Username"]
+        [:input {:type "text"
+                 :id "inputusername"
+                 :placeholder "Username"
+                 :required
+                 :autofocus}]
+        [:label {:for "inputpassword"
+                 :class "sr-only"} "Password"]
+        [:input {:type "password"
+                 :id "inputpassword"
+                 :placeholder "Password"}]
+        [:button.btn.btn-sm.btn-primary {:type "Submit"} "Sign In"]]
+    [:p ]
+    [:p "Please note that all sign in usernames and passwords are temporary."]
+    [:p "To sign in using your account please go to the "
+        [:a {:href "#"} "Login"] " page."]
+   ]])
